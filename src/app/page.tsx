0337e4d353d7c2622b913bc1b396524b7bdd8929
Steven Molina <stevenmolina2205@gmail.com>
2026-01-13 18:21:55 -0300

    chore(): add initial base config

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
 "use client";
 
-import React from "react";
+import { useEffect } from "react";
 import { redirect } from "next/navigation";
+import { sdk } from "@farcaster/miniapp-sdk";
 
 export default function DisputesPage() {
+  useEffect(() => {
+    sdk.actions.ready();
+  }, []);
   redirect("/disputes");
   return <div></div>;
 }
